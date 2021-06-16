5f0d5a729fa7da28673098d2bed191278df98cad
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-16 13:42:11 -0700

    update paths

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,7 +31,7 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns marbles', async() => {
 
       const expectation = [
         {
@@ -55,7 +55,7 @@ describe('app routes', () => {
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/marbles')
         .expect('Content-Type', /json/)
         .expect(200);
 
---
+++ data/marbles.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Crack Attack',
+    image: 'crackattack.png',
+    description: 'Crack attack breaks other marbles in half',
+    category: 'rare',
+    price: 189.00,
+    cost: 16.00
+  },
+    
+  {
+    id: 2,
+    name: 'Basic Blue',
+    image: 'basicblue.png',
+    description: 'Baisc blue has some boring swirls',
+    category: 'common',
+    price: 6.50,
+    cost: 0.25
+  },
+    
+  { 
+    id: 3,
+    name: 'Windy',
+    image: 'windy.png',
+    description: 'Swwoooosh',
+    category: 'uncommon',
+    price: 15.00,
+    cost: 3.00
+  },
+    
+  {
+    id: 4,
+    name: 'catyeye',
+    image: 'catyeye.png',
+    description: 'This ones just gonna knock your stuff down.',
+    category: 'common',
+    price: 50.00,
+    cost: 4.25 
+  },
+    
+  { 
+    id: 5,
+    name: 'Neuron',
+    image: 'neuron.png',
+    description: 'Swwoooosh',
+    category: 'rare',
+    price: 250.00,
+    cost: 45.00
+  }
+];
